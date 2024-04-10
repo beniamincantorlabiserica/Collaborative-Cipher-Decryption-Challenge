3e04b7552042cdbfcbbdc2c2b5cf4df2c6660430
beniamin <avr.beniamin@icloud.com>
2024-04-10 10:45:04 +0200

    UPDATED UI to light theme

--- Collaborative-Cipher-Decryption-Challenge/obj/Debug/net8.0/Collaborative-Cipher-Decryption-Challenge.AssemblyInfo.cs
+++ Collaborative-Cipher-Decryption-Challenge/obj/Debug/net8.0/Collaborative-Cipher-Decryption-Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c878ef3a6d778542cc69ed3b44b3357953e669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e434649e3a9f33294c2b6d6377902f4234ba673")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
