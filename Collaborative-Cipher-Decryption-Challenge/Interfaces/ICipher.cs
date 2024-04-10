72c878ef3a6d778542cc69ed3b44b3357953e669
beniamin <avr.beniamin@icloud.com>
2024-04-10 05:55:31 +0200

    ADDED instructions for args list use for better understanding of the structure of the system

--- Collaborative-Cipher-Decryption-Challenge/Interfaces/ICipher.cs
+++ Collaborative-Cipher-Decryption-Challenge/Interfaces/ICipher.cs
@@ -4,6 +4,11 @@ namespace Collaborative_Cipher_Decryption_Challenge.Interfaces;
 
 public interface ICipher
 {
+    // Keep in mind that the args list is a list of all the arguments that the user has inputted
+    // the structure of the list is as follows:
+    // args[0] = the input from the user (text to be encrypted/decrypted)
+    // args[1] = the first argument of the algorithm (use only if needed, otherwise ignore)
+    // args[2] = the second argument of the algorithm (use only if needed, otherwise ignore)
     public string Decrypt(List<string> args);
     public string Encrypt(List<string> args);
 }
