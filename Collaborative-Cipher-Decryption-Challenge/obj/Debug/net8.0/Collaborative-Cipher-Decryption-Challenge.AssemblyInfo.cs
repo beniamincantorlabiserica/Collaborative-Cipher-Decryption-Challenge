4cc535015e435f3e24b8b7cb1e5459e422ec8cad
beniamin <avr.beniamin@icloud.com>
2024-04-10 04:13:27 +0200

    ADDED UI

--- Collaborative-Cipher-Decryption-Challenge/obj/Debug/net8.0/Collaborative-Cipher-Decryption-Challenge.AssemblyInfo.cs
+++ Collaborative-Cipher-Decryption-Challenge/obj/Debug/net8.0/Collaborative-Cipher-Decryption-Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d12f0ca3215b37ccd85968c99be98092d75d36df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5badc4cbf8431c76b7e0a7aba2218ad7e12dc43d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collaborative-Cipher-Decryption-Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
