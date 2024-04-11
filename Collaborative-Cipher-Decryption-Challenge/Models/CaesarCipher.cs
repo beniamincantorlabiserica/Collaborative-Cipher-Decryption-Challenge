0f4460a77d6caf3ebeb187c66aadfa629cae2668
bob <eduards.br7@gmail.com>
2024-04-11 19:30:39 +0200

    caesar-WIP

--- Collaborative-Cipher-Decryption-Challenge/Models/CaesarCipher.cs
+++ Collaborative-Cipher-Decryption-Challenge/Models/CaesarCipher.cs
@@ -3,15 +3,67 @@ using Collaborative_Cipher_Decryption_Challenge.Interfaces;
 
 namespace Collaborative_Cipher_Decryption_Challenge.Models;
 
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using DynamicData;
+
 public class CaesarCipher : ICipher
 {
-    public string Decrypt(List<string> args)
+
+    private string _plaintext;
+    private string _shift;
+    private string shifted;
+
+    public CaesarCipher()
     {
-        throw new System.NotImplementedException();
+
     }
+
+    private void SetParamenters(List<string> args)
+    {
+        _plaintext = args[0];
+        _shift = args[1];
+    } 
+    
 
     public string Encrypt(List<string> args)
     {
-        throw new System.NotImplementedException();
+        string encryptedText = "";
+        foreach (char c in _plaintext)
+        {
+            if (char.IsLetter(c))
+            {
+                char lowerC = char.ToLower(c);
+                char shifted = (char)(((lowerC + _shift - 'a') % 26) + "a");
+                encryptedText += shifted;
+            }
+            else
+            {
+                encryptedText += c;
+            }   
+        }
+        
+        return encryptedText;
+    }
+
+    public string Decrypt(List<string> args)
+    {
+        string decryptedText = "";
+
+        foreach(char c in _plaintext)
+        {
+            if(char.IsLetter(c))
+            {
+                char lowerC = char.ToLower(c);
+                char shifted = (char)(((lowerC - _shift - 'a' + 26) % 26) + "a");
+                decryptedText += shifted;
+            }
+            else
+            {
+                decryptedText += c;
+            }
+        }
     }
 }
