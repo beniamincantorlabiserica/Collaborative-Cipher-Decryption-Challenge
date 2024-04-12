863c334dcb26e0a707a6d16a4a04b945f2135654
IonelaSorina <ionelapertea24@gmail.com>
2024-04-12 19:02:23 +0200

    ADDED Atbash cipher logic

--- Collaborative-Cipher-Decryption-Challenge/Models/AtbashCipher.cs
+++ Collaborative-Cipher-Decryption-Challenge/Models/AtbashCipher.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Collaborative_Cipher_Decryption_Challenge.Interfaces;
+using DynamicData;
 
 namespace Collaborative_Cipher_Decryption_Challenge.Models;
 
@@ -7,11 +10,59 @@ public class AtbashCipher : ICipher
 {
     public string Decrypt(List<string> args)
     {
-        throw new System.NotImplementedException();
+        string input = args[0];
+        string[] letters =
+        {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"  //26 letters
+        };
+        string output = string.Empty;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char letter = input[i];
+            int indexOfLetter = letters.IndexOf(letter.ToString());
+            if (letter.Equals(letter.ToString().ToLower()))
+            {
+                output += letters[25 - indexOfLetter];
+
+            }
+            else
+            {
+                output += letters[25 - indexOfLetter].ToUpper();
+            }
+        }
+
+        return output;
     }
 
     public string Encrypt(List<string> args)
     {
-        throw new System.NotImplementedException();
+        string input = args[0];
+        string[] letters =
+        {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"  //26 letters
+        };
+        string output = string.Empty;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char letter = input[i];
+            int indexOfLetter = letters.IndexOf(letter.ToString());
+            if (letter.Equals(letter.ToString().ToLower()))
+            {
+                output += letters[25 - indexOfLetter];
+
+            }
+            else
+            {
+                output += letters[25 - indexOfLetter].ToUpper();
+            }
+
+        }   
+
+        return output;
     }
+    
 }
